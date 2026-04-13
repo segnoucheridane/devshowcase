b25d9943a15a5cb86f4cbdcc2aaf094986a1e4b6
segnou cheridane <segnoucheradane@gmail.com>
2026-04-13 10:45:56 +1000

    fix: use api/index.js for Vercel deployment

--- api/index.js
+++ api/index.js
@@ -1,4 +1,9 @@
-const app = require('../src/app');
+const app = require('./src/app');
+
+// Add a direct test route
+app.get('/test', (req, res) => {
+  res.json({ message: 'API is working' });
+});
 
 // Export for Vercel
 module.exports = app;
