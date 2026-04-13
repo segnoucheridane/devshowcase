3036217f3263f359d726ebd25aa7d31e96c3691d
segnou cheridane <segnoucheradane@gmail.com>
2026-04-13 03:31:09 +0100

    fix: change App to app in require path (case sensitive)

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const http = require('http');
 //const { Server } = require('socket.io');
-const app = require('./src/App');
+const app = require('./src/app');
 const { connectDB } = require('./src/config/prisma');
 const notificationService = require('./src/services/NotificationService');
 
